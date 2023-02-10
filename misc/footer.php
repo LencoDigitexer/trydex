cda5da7aa89f047c87532edf10deb1daf78b23c0
LencoDigitexer <addminuser258@gmail.com>
2023-02-10 09:17:36 +0300

    add telegram bot

--- misc/footer.php
+++ misc/footer.php
@@ -2,6 +2,7 @@
     <a href="./">TrydeX</a>
     <a href="./settings.php">Настройки</a>
     <a href="./api.php" target="_blank">API</a>
+    <a href="https://t.me/librex_bot" target="_blank">Поиск в Telegram</a>
 </div>
 
 </body>
